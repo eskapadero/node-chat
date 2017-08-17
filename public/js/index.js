359fa79319a9ac4d107e7cdfb6c7222ec588724c
eskapadero <eskapadero@gmail.com>
2017-08-17 21:48:27 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,7 @@ var socket = io();
 
 socket.on('connect',function ()  {
         console.log('Connected to server');
-    
-        socket.emit('createMessage',{
-           from: 'zyrian',
-           text: 'I love you too'
-            
-        });
+          
       });
       
       socket.on('disconnect',function ()  {
